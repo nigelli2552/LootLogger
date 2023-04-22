f1f478b59a541e7234d758e3184942f941812011
Hui Li <nigelathome@126.com>
2023-04-22 17:35:46 +0800

    add project

---
+++ LootLogger/ItemsViewController.swift
@@ -0,0 +1,12 @@
+//
+//  ItemsViewController.swift
+//  LootLogger
+//
+//  Created by nigelli on 2023/4/22.
+//
+
+import Foundation
+import UIKit
+
+class ItemsViewController: UITableViewController {
+}
---
+++ LootLogger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LootLogger
+//
+//  Created by nigelli on 2023/4/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
