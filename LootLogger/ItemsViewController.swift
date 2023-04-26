4fd6de46ac63b2504a6e629384e5b0570dfc4b8a
Hui Li <nigelathome@126.com>
2023-04-27 07:24:46 +0800

    dequeue resuable cell

--- LootLogger/ItemsViewController.swift
+++ LootLogger/ItemsViewController.swift
@@ -17,7 +17,8 @@ class ItemsViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         // Create an instance of UITableViewCell with default appearance
-        let cell = UITableViewCell(style: .value1, reuseIdentifier: "UITableViewCell")
+//        let cell = UITableViewCell(style: .value1, reuseIdentifier: "UITableViewCell")
+        let cell=tableView.dequeueReusableCell(withIdentifier: "UITabelViewCell", for: indexPath)
 
         // Set the text on the cell with the description of the item, that is at nth index of items, where n = row this cell, will appear in on the table view
         let item = itemStore.allItems[indexPath.row]
