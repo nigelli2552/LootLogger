1570e976d3a6c53b035873c3d6c31c959377f85c
Hui Li <nigelathome@126.com>
2023-04-26 08:55:37 +0800

    cell for displaying items.

--- LootLogger/ItemsViewController.swift
+++ LootLogger/ItemsViewController.swift
@@ -10,7 +10,21 @@ import UIKit
 
 class ItemsViewController: UITableViewController {
     var itemStore: ItemStore!
-    override func numberOfSections(in tableView: UITableView) -> Int {
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemStore.allItems.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // Create an instance of UITableViewCell with default appearance
+        let cell = UITableViewCell(style: .value1, reuseIdentifier: "UITableViewCell")
+
+        // Set the text on the cell with the description of the item, that is at nth index of items, where n = row this cell, will appear in on the table view
+        let item = itemStore.allItems[indexPath.row]
+
+        cell.textLabel?.text = item.name
+        cell.detailTextLabel?.text = "$\(item.valueInDollars)"
+
+        return cell
     }
 }
