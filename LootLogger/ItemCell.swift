597747165a28ceb9043284ce5eecd5d1064f6c80
Hui Li <nigelathome@126.com>
2023-05-10 08:17:48 +0800

    customize the item cell

---
+++ LootLogger/ItemCell.swift
@@ -0,0 +1,15 @@
+//
+//  ItemCell.swift
+//  LootLogger
+//
+//  Created by nigelli on 2023/5/10.
+//
+
+import Foundation
+import UIKit
+
+class ItemCell: UITableViewCell {
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var serialNumberLabel: UILabel!
+    @IBOutlet var valueLabel: UILabel!
+}
--- LootLogger/ItemsViewController.swift
+++ LootLogger/ItemsViewController.swift
@@ -38,20 +38,26 @@ class ItemsViewController: UITableViewController {
         }
     }
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.rowHeight = 65
+    }
+
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemStore.allItems.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         // Create an instance of UITableViewCell with default appearance
-//        let cell = UITableViewCell(style: .value1, reuseIdentifier: "UITableViewCell")
-        let cell = tableView.dequeueReusableCell(withIdentifier: "UITabelViewCell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ItemCell", for: indexPath) as! ItemCell
 
         // Set the text on the cell with the description of the item, that is at nth index of items, where n = row this cell, will appear in on the table view
         let item = itemStore.allItems[indexPath.row]
 
-        cell.textLabel?.text = item.name
-        cell.detailTextLabel?.text = "$\(item.valueInDollars)"
+        // Configure the cell with the Item
+        cell.nameLabel.text = item.name
+        cell.serialNumberLabel.text = item.serialNumber
+        cell.valueLabel.text = "$\(item.valueInDollars)"
 
         return cell
     }
