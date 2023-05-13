a93a65963628a830c95f916b269f8a561fb26d7c
Hui Li <nigelathome@126.com>
2023-05-13 10:15:47 +0800

    Update the Item value

--- LootLogger/ItemsViewController.swift
+++ LootLogger/ItemsViewController.swift
@@ -43,6 +43,11 @@ class ItemsViewController: UITableViewController {
         tableView.rowHeight = UITableView.automaticDimension
         tableView.estimatedRowHeight = 65
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        tableView.reloadData()
+    }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemStore.allItems.count
