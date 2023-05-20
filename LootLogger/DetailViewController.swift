4580b75eb78ad48195c56de473d150e24ae42d1e
Hui Li <nigelathome@126.com>
2023-05-20 10:35:43 +0800

    modal design: set the modal presentation style

--- LootLogger/DetailViewController.swift
+++ LootLogger/DetailViewController.swift
@@ -16,14 +16,19 @@ class DetailViewController: UIViewController, UITextFieldDelegate {
 
     @IBAction func choosePhotoSource(_ sender: UIBarButtonItem) {
         let alertController = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        alertController.modalPresentationStyle = .popover
+        alertController.popoverPresentationController?.barButtonItem = sender
+
         let cameraAction = UIAlertAction(title: "Camera", style: .default) { _ in
             print("Present camera")
         }
         alertController.addAction(cameraAction)
+
         let photoLibraryAction = UIAlertAction(title: "Photo Library", style: .default) { _ in
             print("Present photo library")
         }
         alertController.addAction(photoLibraryAction)
+
         let cancelAction = UIAlertAction(title: "Cancel", style: .default, handler: nil)
         alertController.addAction(cancelAction)
 
