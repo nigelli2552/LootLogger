5acce618325e23ac93396490aece79cf02c8006f
Hui Li <nigelathome@126.com>
2023-05-27 08:39:41 +0800

    load the image from imageStore

--- LootLogger/DetailViewController.swift
+++ LootLogger/DetailViewController.swift
@@ -44,6 +44,13 @@ class DetailViewController: UIViewController, UITextFieldDelegate, UINavigationC
         serialNumberField.text = item.serialNumber
         valueField.text = numberFormatter.string(from: NSNumber(value: item.valueInDollars))
         dateLabel.text = dateFormatter.string(from: item.dateCreated)
+
+        // Get the item key
+        let key = item.itemKey
+
+        // If there is an associated image with the item, display it on the image view
+        let imageToDisplay = imageStore.image(forKey: key)
+        imageView.image = imageToDisplay
     }
 
     override func viewWillDisappear(_ animated: Bool) {
